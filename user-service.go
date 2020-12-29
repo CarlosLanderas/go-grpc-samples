966d0e1fe24a70310d5ee87f393d5006bd089bc1
Carlos Landeras <clanderas@plainconcepts.com>
2020-12-29 16:10:29 +0100

    add user service interface

---
+++ user-service.go
@@ -0,0 +1,15 @@
+package userservice
+
+import "errors"
+
+var ErrNotFound = errors.New("user not found")
+
+type User struct {
+	Id int64
+	Name string
+}
+
+type IUserService interface {
+	GetUser(id int64) (User, error)
+	GetUsers(ids []int64) (map[int64]User, error)
+}
