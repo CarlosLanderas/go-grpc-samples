7eb77d8ee4b80e569c040723118ed1972a8457df
Carlos Landeras <carlos.landeras@gmail.com>
2020-12-29 16:58:55 +0100

    Add get users

--- main.go
+++ main.go
@@ -19,5 +19,4 @@ func main() {
 
 	db.OpenDb()
 
-
 }
--- user-service/user-service.go
+++ user-service/user-service.go
@@ -21,10 +21,28 @@ type UserService struct {
 	dbClient dbclient.BoltClient
 }
 
-func (us *UserService) GetUser(id int64) (User, error) {
-	us.dbClient.
-}
-
 func NewUserService(dbClient dbclient.BoltClient) UserService {
 	return UserService{dbClient}
+}
+
+func (us *UserService) GetUser(id int64) (User, error) {
+	return us.dbClient.GetUser(id)
+}
+
+func (us *UserService) GetUsers(ids []int64) (map[int64]User, error) {
+
+	users := map[int64]User{}
+
+	for _ , id := range ids {
+
+		user, err := us.dbClient.GetUser(id)
+
+		if err != nil {
+			return users, err
+		}
+
+		users[id] = user
+	}
+
+	return users, nil
 }
