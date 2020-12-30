4acf31562eb1893aeec67de654c609433f9d82d1
Carlos Landeras <carlos.landeras@gmail.com>
2020-12-30 01:21:51 +0100

    Merge benches

--- benches_test.go
+++ benches_test.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"go-grpc-samples/dbclient"
 	userservicegrpc "go-grpc-samples/grpc"
+	"go-grpc-samples/http"
 	"google.golang.org/grpc"
+	net "net/http"
 	"testing"
 )
 
+var db dbclient.BoltClient
+
 func init() {
-	db := dbclient.GetDatabase()
+	db = dbclient.GetDatabase()
 	db.OpenDb()
 	go userservicegrpc.Start(":8000", db)
+	go func() {
+		server := http.NewServer(db)
+		server.Start(":8001")
+	}()
 }
 
 
@@ -25,5 +35,15 @@ func BenchmarkGrpcService(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		ids := []int64{8,6,70}
 		client.GetUsers(context.Background(), &userservicegrpc.GetUsersRequest{Ids: ids })
+	}
+}
+
+func BenchmarkHttpService(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ids := []int64{8,6,70}
+		content, _ := json.Marshal(ids)
+		reader := bytes.NewReader(content)
+
+		net.DefaultClient.Post("http://localhost:8001/users", "application/json", reader)
 	}
 }
